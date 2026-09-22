453f859d0293e1b44634cf8d705338b0642896dd
n-Batman <nlvatman1@gmail.com>
2026-09-21 19:06:39 -0700

    removed previous test code

--- .vscode/i2LoggerProject/canTest.py
+++ .vscode/i2LoggerProject/canTest.py
@@ -4,16 +4,7 @@ import fakeBLFdata
 
 from pathlib import Path
 
-log = fakeBLFdata.create_blf(Path("drive.blf"), 1)
-
-with can.BLFReader("drive.blf") as log:
-    for message in log:
-        print(
-            f"{message.timestamp:.6f}  "
-            f"channel={message.channel}  "
-            f"id=0x{message.arbitration_id:X}  "
-            f"data={message.data.hex(' ')}"
-        )
+log = fakeBLFdata.create_blf(Path("drive.blf"), 2)
 
 
 with open("drive.csv", "w", newline="") as csvfile:
