07221b48c5c239bb86e9b14fa32f128303ea0952
n-Batman <nlvatman1@gmail.com>
2026-09-23 19:51:13 -0700

    Created shell of create_csv function

--- .vscode/i2LoggerProject/canTest.py
+++ .vscode/i2LoggerProject/canTest.py
@@ -1,10 +1,55 @@
 import can
 import csv
 import fakeBLFdata
+import cantools
 
 from pathlib import Path
 
+db = cantools.database.load_file('C:\\Users\\nlvat\\vscode-python-projects\\can.dbc')
+db.messages
+
 log = fakeBLFdata.create_blf(Path("drive.blf"), 2)
+
+def create_csv(blf_path, dbc_message, csv_path = "drive.csv"):
+    #Takes in the DBC message and BLF, and turns the BLF to a csv using DBC as collumns. 
+    signal_names = [signal.name for signal in dbc_message.signals]
+
+    with open(csv_path, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer = csv.DictWriter(
+            csvfile,
+            fieldnames=["timestamp", "channel", "id", *signal_names],
+            extrasaction="ignore",
+        )
+        writer.writeheader()
+
+        with can.BLFReader(blf_path) as log:
+            for frame in log:
+
+                if frame.arbitration_id != dbc_message.frame_id:
+                    continue
+
+                try:
+                    decoded_signals = dbc_message.decode(
+                        frame.data,
+                        decode_choices=True,
+                    )
+                except Exception as error:
+                    print(
+                        f"Skipping invalid frame at {frame.timestamp:.6f}: {error}"
+                    )
+                    continue
+
+                
+                writer.writerow(
+                    {
+                        "timestamp": f"{frame.timestamp:.6f}",
+                        "channel": frame.channel,
+                        "id": f"0x{frame.arbitration_id:X}",
+                        **decoded_signals,
+                    }
+                )
+
 
 
 with open("drive.csv", "w", newline="") as csvfile:
@@ -20,4 +65,4 @@ with open("drive.csv", "w", newline="") as csvfile:
                     message.data.hex(" "),
                 ]
             )
-
+# probably use Can Bus Tools python to add DBC files to the can bus data. (adds units)
